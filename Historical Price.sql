dbe07968b03b3a85e55797ba2e8146569ce8e198
Dung Nguyen <ng.tridung97@gmail.com>
2020-05-21 23:46:20 +0700

    Add second approach, using lag and self join functions

--- Historical Price.sql
+++ Historical Price.sql
@@ -108,3 +108,76 @@ vendor_num|item_idnt|item_desc                                 |min_doc_date|max
 6021709   |  3169697|ORGANIC FREE RANGE EGGS 12PK 600G:600 GRAM|  2019-01-02|  2019-01-09|   $87.00|
 
 At first glance, it's okay. But then we should notice item '2680008' is overlapped in date range because of the repeat of price $50.50. Therefore, this method can't tell the Manager the correct answer. */
+
+
+-- 2. Sencond approach is lag() or lead() window function
+
+with
+
+	source_table as (select vendor_num, item_idnt, item_desc, doc_date, inv_price,
+		
+			lag(inv_price) over(partition by vendor_num, item_idnt order by doc_date) as prev_inv_price,
+			
+			lag(doc_date) over(partition by vendor_num, item_idnt order by doc_date) as to_date
+	
+		from public.invoice_detail),
+		
+	max_doc_date as (select vendor_num, item_idnt, max(doc_date) as max_doc_date
+
+		from public.invoice_detail	
+	
+		group by vendor_num, item_idnt),
+
+	price_change as (select vendor_num, item_idnt, item_desc, to_date, doc_date, prev_inv_price, inv_price,
+
+			(inv_price/prev_inv_price - 1) as perc_variance,
+			
+			case
+			
+			when (inv_price/prev_inv_price - 1) = 0 then 0
+				
+			else 1 end as classification
+
+		from source_table),
+	
+	before_offset as (select vendor_num, item_idnt, item_desc, to_date, doc_date, prev_inv_price, inv_price, perc_variance
+
+		from price_change
+
+		where classification = 1),
+	
+	after_offset as (select *, row_number() over(partition by vendor_num, item_idnt order by doc_date) as rn
+			
+		from before_offset)
+		
+select
+
+	a.vendor_num, a.item_idnt, a.item_desc,
+	
+	a.doc_date as from_date, coalesce(b.to_date, c.max_doc_date) as to_date, a.inv_price, a.prev_inv_price, a.perc_variance
+
+from after_offset a
+
+left join after_offset b
+
+on a.vendor_num = b.vendor_num and a.item_idnt = b.item_idnt and a.rn = b.rn - 1
+
+left join max_doc_date c
+
+on a.vendor_num = c.vendor_num and a.item_idnt = c.item_idnt;
+
+/* Result
+
+vendor_num|item_idnt|item_desc                                 |from_date |to_date   |inv_price|prev_inv_price|perc_variance      |
+----------|---------|------------------------------------------|----------|----------|---------|--------------|-------------------|
+6021709   |  1034234|JUMBO FREE RANGE EGGS 12 PACK:800 GRAM    |2019-01-02|2019-01-07|   $55.00|              |                   |
+6021709   |  1034234|JUMBO FREE RANGE EGGS 12 PACK:800 GRAM    |2019-01-08|2019-01-12|   $48.25|        $55.00|-0.1227272727272727|
+6021709   |  1034234|JUMBO FREE RANGE EGGS 12 PACK:800 GRAM    |2019-01-15|2019-01-22|   $50.75|        $48.25| 0.0518134715025906|
+6021709   |  2680008|FREE RANGE EGGS 6 PACK 300G:300 GRAM      |2019-01-04|2019-01-11|   $47.45|              |                   |
+6021709   |  2680008|FREE RANGE EGGS 6 PACK 300G:300 GRAM      |2019-01-16|2019-01-31|   $50.50|        $47.45| 0.0642781875658589|
+6021709   |  2680008|FREE RANGE EGGS 6 PACK 300G:300 GRAM      |2019-02-04|2019-02-07|   $59.25|        $50.50| 0.1732673267326732|
+6021709   |  2680008|FREE RANGE EGGS 6 PACK 300G:300 GRAM      |2019-02-08|2019-02-14|   $50.50|        $59.25|-0.1476793248945147|
+6021709   |  3169697|ORGANIC FREE RANGE EGGS 12PK 600G:600 GRAM|2019-01-02|2019-01-09|   $87.00|              |                   |
+6021709   |  3169697|ORGANIC FREE RANGE EGGS 12PK 600G:600 GRAM|2019-01-10|2019-01-16|   $85.50|        $87.00|-0.0172413793103449|
+
+Note: In this case, we should use lag() to determine the previous row, then "remove" if price in that rows are equaled to previous one. Finally, self join with row number (rn -1) and we will get a result that solve the Manager request. */
