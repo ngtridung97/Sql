68339763eb1255f4ab4ba8f5e94ed9af5f9ab379
Dung Nguyen <ng.tridung97@gmail.com>
2020-05-24 11:46:09 +0700

    Add show running queries

--- PostgreSQL Monitoring.sql
+++ PostgreSQL Monitoring.sql
@@ -15,3 +15,21 @@ from information_schema.columns a
 inner join pg_stat_user_tables b
 
 on a.table_schema = b.schemaname and a.table_name = b.relname;
+
+-- 2. Running queries
+
+select
+
+	a.pid, age(clock_timestamp(), a.query_start), a.usename, a.query,
+
+	b.mode, b.locktype, b.granted
+	
+from pg_stat_activity a
+
+inner join pg_locks b
+
+on a.pid = b.pid 
+
+where a.query != '<IDLE>' and a.query not like '%pg_stat_activity%'
+
+order by b.granted, b.pid desc;
